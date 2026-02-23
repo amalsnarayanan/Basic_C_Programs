20c9d36aa1d71aebdc4bf0438a716af776b1cbbf
amalsnarayanan <85282454+amalsnarayanan@users.noreply.github.com>
2026-02-23 17:11:13 +0530

    Add files via upload
    Find largest of three numbers

---
+++ largest.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+  int n1,n2,n3;
+  printf("Enter the numbers : ");
+  scanf("%d %d %d", &n1, &n2, &n3);
+  
+  if(n1>n2 && n1>n3)
+  {
+     printf("%d is largest", n1);
+  }
+
+  else if(n2>n3)
+  {
+     printf("%d is largest", n2);       
+    
+  }
+  else 
+  {
+
+    printf("%d is largest\n", n3);
+  }
+
+  return 0;
+}
